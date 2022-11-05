595fdba981265b5fcc631bf8510fe2a47b8616ce
olicauli <olicauli@protonmail.com>
2022-11-05 15:25:40 -0500

    update package.json, commands

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-//commands composed of two members:
-//data, which provides the command definition for registering to discord
-//execute, which contains the functionality to run from the event handler
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,22 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`ready! logged in as ${client.user.tag}`);
+
+        const job = new CronJob('00 10 * * MON', async () => {
+            const guild = client.guilds.cache.get(GUILD_ID);
+            if (!guild) {
+              return;
+            }
+            const channel = guild.channels.cache.get(CHANNEL_ID);
+            if (!channel || channel.type != 'GUILD_TEXT') {
+              return;
+            }
+            await channel.send({
+              content: "It's Me and Michael Monday",
+              files: ['meandmichael.mp4']
+            });
+          }, null, true, 'America/Chicago');
+          job.start();
+
     },
 };
