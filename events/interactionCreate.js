80f2fa73c4b933c64ee39edab357e0af5f1c336e
olicauli <olicauli@protonmail.com>
2022-11-05 14:08:31 -0500

    modularize

---
+++ events/interactionCreate.js
@@ -0,0 +1,22 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+    name: Events.InteractionCreate,
+    async execute(interaction)
+    {
+        if (!interaction.isChatInputCommand()) return; //if it's not a slash command, return
+        const command = interaction.client.commands.get(interaction.commandName);
+        if (!command)
+        {
+            console.error(`no command matching ${interaction.commandName} was found`);
+            return;
+        }
+        try {
+            await command.execute(interaction);
+        } catch(error) {
+            console.error(error);
+            await interaction.reply({ content: "there was an error. oopsie", ephemeral: true});
+        }
+        console.log(`${interaction.commandName} executed!`);
+    }
+}
---
+++ helpers/command-handler.js
@@ -0,0 +1,23 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Collection } = require('discord.js');
+
+
+function handleCommand(client)
+{
+    client.commands = new Collection();
+    const commandsPath = path.join(__dirname, '../commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] the command at ${filePath} is missing a required "data" or "execute" property`);
+        }
+    }
+}
+
+module.exports = { handleCommand };
